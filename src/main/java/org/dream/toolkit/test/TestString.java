7aedeee8b39b9bf8fd5687cfd9e10c48e9e88c08
雷亚锋 <lyfgyyhp@163.com>
2019-01-23 10:20:25 +0800

    提交

---
+++ src/main/java/org/dream/toolkit/test/TestString.java
@@ -0,0 +1,10 @@
+package org.dream.toolkit.test;
+
+import org.dream.toolkit.common.StringUtils;
+
+public class TestString {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.createRandom(1));
+        System.out.println(StringUtils.camelcase("stuid"));
+    }
+}
