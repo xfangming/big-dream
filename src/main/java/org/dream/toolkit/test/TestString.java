176b723abe255717a5ac716b99a1c16ca2202773
雷亚锋 <lyfgyyhp@163.com>
2019-01-23 11:05:30 +0800

    提交

--- src/main/java/org/dream/toolkit/test/TestString.java
+++ src/main/java/org/dream/toolkit/test/TestString.java
@@ -6,5 +6,6 @@ public class TestString {
     public static void main(String[] args) {
         System.out.println(StringUtils.createRandom(1));
         System.out.println(StringUtils.camelcase("stuid"));
+        System.out.println(StringUtils.camelcase("stuid1"));
     }
 }
